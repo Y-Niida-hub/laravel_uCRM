828128f7d0ec71438fcaef052d9b270a8a9a69a8
Y Niida <gc.sapporo.niida@gmail.com>
2025-08-13 13:41:23 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Lavel from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+</script>
+
+<template>
+<GuestLayout>
+<Lavel value="件名">タイトル</Lavel>
+<Input 
+model-value="初期値が入ります"
+@update:model-value="emitTest"
+></Input>
+</GuestLayout>
+
+
+</template>
